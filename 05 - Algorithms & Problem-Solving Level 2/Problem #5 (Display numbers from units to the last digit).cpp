60ed4717c2a010d191724c3a2a9ef6c1308bf2f7
PriceElio <49208117+enderbrine13@users.noreply.github.com>
2024-07-01 12:49:59 +0300

    Create Problem #5 (Display numbers from units to the last digit).cpp

---
+++ 05 - Algorithms & Problem-Solving Level 2/Problem #5 (Display numbers from units to the last digit).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int inputNum(){ //You can make a check if the number is positive if you want, I just tried to simplify it :D )
+    int N;
+    cin>>N;
+    return N;
+}
+
+int getUnitDigit(int& num){  //Passing by reference
+    int unit = num%10;
+    num/=10;
+    return unit;
+}
+
+void displayDigitsFromUnits(int num){
+    while (num) {
+        cout<<getUnitDigit(num)<<endl;
+    }
+}
+
+int main()
+{
+    displayDigitsFromUnits(inputNum());
+}
